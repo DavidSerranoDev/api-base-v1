24a912505ceb13092fe50e9855fda1c952f78de7
david.serrano <david.serrano.dev@gmail.com>
2023-12-09 11:30:44 +0100

    users entity

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,31 @@
-export class User {}
+import { Column, CreateDateColumn, DeleteDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+@Entity()
+export class User {
+    @PrimaryGeneratedColumn()
+    id:number;
+    
+    @Column()
+    name:string;
+
+    @Column({unique:true, nullable:false})
+    email:string;
+
+    @Column()
+    phone_number:number;
+
+    @Column({nullable: false})
+    password:string;
+
+    @Column( {default: 'user'})
+    role:string;
+
+    @CreateDateColumn()
+    created:Date;
+
+    @DeleteDateColumn()
+    deleted:Date;
+
+    @UpdateDateColumn()
+    modified:Date;
+}
