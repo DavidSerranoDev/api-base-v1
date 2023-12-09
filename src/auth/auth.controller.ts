db7f2c832f0a59bc476f0f3aa688e697c5c6f131
david.serrano <david.serrano.dev@gmail.com>
2023-12-09 13:22:34 +0100

    auth validation done

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { RegisterDto } from './dto/register.dto';
 import { LoginDto } from './dto/login.dto';
@@ -27,7 +27,10 @@ export class AuthController {
      
     @Get('profile')
     @UseGuards(AuthGuard)
-    profile(){
-        return 'Profile';
+    profile(
+        @Request()
+        req
+        ){
+        return req.user;
     }
 }
